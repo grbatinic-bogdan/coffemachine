254e13ef501cfcf1e6334d8895e5356f6df32fa4
grbatinic.bogdan <grbatinic.bogdan@gmail.com>
2019-09-14 16:03:54 +0300

    Add reducer hook for form data

--- src/components/BeverageResponse/index.js
+++ src/components/BeverageResponse/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { isSuccess, notAsked } from '../../services/remoteData'
+import { RESET_COFFEE_MACHINE_ACTION } from '../Form/reducer'
 
-export default ({ beverageData, imageData, setFormData, setImageData }) => {
-  const shouldRender = beverageData && imageData
+export default ({ beverageData, imageRemoteData, dispatch, setImageData }) => {
+  const shouldRender = beverageData && isSuccess(imageRemoteData)
   if (!shouldRender) {
     return null
   }
+  const { data: imageData } = imageRemoteData
   return (
     <div>
       <h1>You ordered {beverageData.beverage.name}</h1>
@@ -35,8 +38,8 @@ export default ({ beverageData, imageData, setFormData, setImageData }) => {
       </p>
       <button
         onClick={() => {
-          setFormData(null)
-          setImageData(null)
+          dispatch({ type: RESET_COFFEE_MACHINE_ACTION })
+          setImageData(notAsked())
         }}
       >
         Order another one
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import Form from '../Form'
 import BeverageResponse from '../BeverageResponse'
 import './style.css'
+import { notAsked } from '../../services/remoteData'
+import { formDataReducer, initialState } from '../Form/reducer'
 
 export default ({ items }) => {
-  const [formData, setFormData] = useState(null)
-  const [imageData, setImageData] = useState(null)
+  const [formData, dispatch] = useReducer(formDataReducer, initialState)
+  const [imageData, setImageData] = useState(notAsked())
   return (
     <div className="app-container">
-      {imageData === null && formData === null && (
-        <Form items={items} setFormData={setFormData} setImageData={setImageData} />
-      )}
+      <Form
+        items={items}
+        setImageData={setImageData}
+        imageData={imageData}
+        formData={formData}
+        dispatch={dispatch}
+      />
       <BeverageResponse
         beverageData={formData}
-        imageData={imageData}
-        setFormData={setFormData}
+        imageRemoteData={imageData}
         setImageData={setImageData}
+        dispatch={dispatch}
       />
     </div>
   )
